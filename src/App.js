78972879bb132c9ac73b2ed51660caaa47125115
miaocc <miaocc@asiainfo.com>
2026-02-14 16:35:47 +0800

    学习

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 
 const str = 'Hello, React!';
 const add = (a, b) => a + b;
@@ -31,8 +31,66 @@ function App() {
           <li key={item.id}>{item.name}</li>
         ))}
       </ul>
+      {/* 实现条件渲染 */}
+      {arr.length > 2 && <p>There are more than 2 items in the array.</p>}
+      {arr.length > 0 ? (
+        <span>There are {arr.length} items in the array.</span>
+      ) : (
+        <p>The array is empty.</p>
+      )}
+      {/* 把 handleClick 这个函数的**本体（引用）**交给了 React。 */}
+      <button onClick={handleClick}>Click me</button>
+      {/* function() 会立刻执行 并将结果返回 所以这里需要用lambda传递函数本身 */}
+      <button onClick={() => handleClick1('Bob')}>Click me too</button>
+      <button onClick={(event) => handleClick2('Alice', event)}>Click me too</button>
+      <Counter className="my-counter" string={str}/>
+      <ExitObject/>
     </div>
   );
 }
 
+const handleClick = (event) => {
+  console.log('Button clicked:', event);
+  // alert('Button was clicked!');
+}
+
+  const handleClick1 = (name) => {
+    console.log('Button clicked:', name);
+  }
+const handleClick2 = (name, event) => {
+  console.log('Button clicked:', name, event);
+  // alert(`Hello, ${name}!`);
+}
+
+// 函数自定义组件 使用useState 进行状态管理
+const Counter = (props) => {
+  const [count, setCount] = useState(0);
+  return (
+    <div className={props.className}>
+      <p className='subP'>Count: {count}</p>
+      <p>{props.string}</p>
+      {/* 使用新值替换旧值 直接修改count 无法渲染到视图 */}
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+      <button onClick={() => setCount(count - 1)}>Decrement</button>
+    </div>
+  );
+}
+
+const ExitObject = () => {
+  const [obj, setObj] = useState({name: 'Alice', age: 25});
+  const updateName = () => {
+    // setObj({name: 'Bob'});  // 这种写法会覆盖掉age属性
+    setObj(prevObj => ({...prevObj, age: prevObj.age + 1})); // 使用函数式更新 保留其他属性
+    // setObj({
+    //   ...obj,
+    //   age: obj.age + 1,
+    // })
+  }
+  return (
+    <div>
+      <p>Name: {obj.name}, Age: {obj.age}</p>
+      <button onClick={updateName}>Update Age</button>
+    </div>
+  );
+}
 export default App;
